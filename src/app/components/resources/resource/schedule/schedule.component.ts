72f05e221d468c696340511cf3233bed7011b1d5
SemenykMykolaWorks <kolya17364654@gmail.com>
2020-11-13 21:59:47 +0200

    change line chart

--- src/app/components/resources/resource/schedule/schedule.component.ts
+++ src/app/components/resources/resource/schedule/schedule.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import * as moment from 'moment';
 
-import { AuthService } from '../../../../services/auth.service';
-import { LineConfigModel, LineValuesModel } from './line-chart/charts.model';
-import { COLORS_MAP, HOUR_IN_MILLISECONDS } from './line-chart/line-chart.config';
-import { CHARGE_SCHEDULE } from './line-chart-data';
+import {AuthService} from '../../../../services/auth.service';
+import {LineConfigModel, LineValuesModel} from './line-chart/charts.model';
+import {COLORS_MAP, HOUR_IN_MILLISECONDS} from './line-chart/line-chart.config';
+import {CHARGE_SCHEDULE} from './line-chart-data';
 
 @Component({
   selector: 'app-schedule',
@@ -18,13 +18,17 @@ export class ScheduleComponent implements OnInit {
   /** Schedule Performance line chart configuration. */
   public schedulePerformanceConfigs: LineConfigModel = {
     showSecondary: true,
-    primaryPostfix: '%',
-    primaryMax: 100,
+    showOppositeSecondary: true,
+    secondaryPostfix: '%',
+    secondaryMax: 100,
+    oppositeSecondaryMax: 10000,
     chartTitle: 'First line chart',
-    primaryYaxisTitle: 'SOC, %',
-    secondaryYaxisTitle: 'Charging Power, kW'
+    primaryYaxisTitle: 'CO2, kg/kWh',
+    secondaryYaxisTitle: 'SOC, %',
+    oppositeSecondaryYaxisTitle: 'Charging Power, kW'
   };
   @Input() battery: any;
+  @Input() intervals: any;
 
   constructor(private activatedRoute: ActivatedRoute,
               private authService: AuthService) {
@@ -34,24 +38,30 @@ export class ScheduleComponent implements OnInit {
     const data = this.convertData();
 
     this.schedulePerformanceValues = [{
+      label: 'CO2 emission',
+      color: COLORS_MAP.ORANGE,
+      type: 'line',
+      data: this.generateMoers().sort((a, b) => a[0] - b[0]),
+      axis: 0
+    }, {
       label: 'SOC',
       color: COLORS_MAP.BLUE,
       type: 'line',
       data: this.generateSOC().sort((a, b) => a[0] - b[0]),
-      axis: 0
+      axis: 1
     }, {
       label: 'Power',
       type: 'area',
       color: COLORS_MAP.YELLOW,
       data: this.generatePower(),
-      axis: 1
+      axis: 2
     }];
   }
 
   public convertData(): number[] {
     const arrayMinutes = [];
     for (let mSecond = CHARGE_SCHEDULE.moers.start; mSecond < CHARGE_SCHEDULE.moers.stop; mSecond = mSecond + HOUR_IN_MILLISECONDS) {
-      arrayMinutes.push( moment(mSecond).format('LT'));
+      arrayMinutes.push(moment(mSecond).format('LT'));
     }
     return arrayMinutes;
   }
@@ -81,18 +91,22 @@ export class ScheduleComponent implements OnInit {
     return costSoc;
   }
 
-  // generateCost() {
-  //   const costArray = [];
-  //   CHARGE_SCHEDULE.intervals.map(item => costArray.push(item.price));
-  //   console.log('generateCost: ' + costArray);
-  //   return costArray;
-  // }
-
-  // generateMoers() {
-  //   let moers = [];
-  //   moers = CHARGE_SCHEDULE.moers.values;
-  //   console.log('generateMoers: ' + moers);
-  //   return moers;
-  // }
-
+  public generateMoers(): number[][] {
+    let moers = [];
+    let date =  CHARGE_SCHEDULE.moers.start;
+    CHARGE_SCHEDULE.moers.values.forEach( value => {
+      moers.push([date, value / 1000]);
+      date = date + (1000 * 60 * 5);
+    });
+    return moers;
+  }
 }
+
+// generateCost() {
+//   const costArray = [];
+//   CHARGE_SCHEDULE.intervals.map(item => costArray.push(item.price));
+//   console.log('generateCost: ' + costArray);
+//   return costArray;
+// }
+
+
