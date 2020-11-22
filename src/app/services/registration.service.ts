f146580f3fa3775e94cdc10ace9c50fceb66f0e1
Kate Negrienko <katya@negrienko.com>
2020-11-22 14:10:45 +0200

    Add Auth Service

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -1,30 +1,37 @@
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {request} from "../constants/api";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { request } from '../constants/api';
+import { LoginRequest } from '../data/LoginRequest';
 
 @Injectable()
 export class RegistrationService {
   constructor(private http: HttpClient) {}
 
-  public apiConstants = request;
-
-  singUp(body) {
-    return this.http.post(`${this.apiConstants.apiUrl}${this.apiConstants.signup}`, body, {observe: 'response'})
+  singUp(body: LoginRequest) {
+    return this.http.post(`${request.apiUrl}${request.signup}`, body, {
+      observe: 'response',
+    });
   }
 
-  authenticate(body) {
-    return this.http.post(`${this.apiConstants.apiUrl}${this.apiConstants.authenticate}`, body, {observe: 'response'})
+  authenticate(body: LoginRequest) {
+    return this.http.post(`${request.apiUrl}${request.authenticate}`, body, {
+      observe: 'response',
+    });
   }
 
   googleLogin() {
-    return this.http.get(`${this.apiConstants.apiUrl}${this.apiConstants.signupGoogle}`)
+    return this.http.get(`${request.apiUrl}${request.signupGoogle}`);
   }
 
   authrized(code: string) {
-    return this.http.get(`${this.apiConstants.apiUrl}${this.apiConstants.authrized}?code=${code}`)
+    return this.http.get(`${request.apiUrl}${request.authrized}?code=${code}`);
   }
 
   googleAuthenticate(code: string) {
-    return this.http.post(`${this.apiConstants.apiUrl}googleAuthenticate?code=${code}`, {}, {observe: 'response'})
+    return this.http.post(
+      `${request.apiUrl}googleAuthenticate?code=${code}`,
+      {},
+      { observe: 'response' }
+    );
   }
 }
