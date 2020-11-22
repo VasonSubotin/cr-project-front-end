35f29bd7258bed27f109bda6386e7dcd6ec2133d
Kate Negrienko <katya@negrienko.com>
2020-11-22 19:04:04 +0200

    add login input styles

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,19 +1,33 @@
-import {Component, OnDestroy} from '@angular/core';
-import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from "@angular/forms";
-import {ErrorStateMatcher} from "@angular/material/core";
-
-import {AuthService} from "../../services/auth.service";
-import {Router} from "@angular/router";
-import {catchError, tap} from 'rxjs/operators';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs/internal/observable/of';
+
 import { MySnackbarService } from 'src/app/services/snackbar.service';
 import { RegistrationService } from 'src/app/services/registration.service';
+import { LoginRequest } from 'src/app/data/LoginRequest';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
@@ -22,64 +36,67 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss'],
 })
-
-
 export class RegistrationComponent implements OnDestroy {
-  constructor(private _registrationService: RegistrationService,
-              private _snackBar: MySnackbarService,
-              private _router: Router) {}
+  constructor(
+    private _registrationService: RegistrationService,
+    private _snackBar: MySnackbarService,
+    private _router: Router
+  ) {}
 
   private subscriptions$ = [];
 
   myGroup = new FormGroup({
-    login: new FormControl('', [
-      Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-    ]),
-    confirmPass: new FormControl('', [
-      Validators.required,
-    ])
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPass: new FormControl('', [Validators.required]),
   });
-
 
   matcher = new MyErrorStateMatcher();
 
-
   onSubmit() {
-    const body = {
+    const body: LoginRequest = {
       login: this.myGroup.controls['login'].value,
       password: this.myGroup.controls['password'].value,
     };
-    const request$ = this._registrationService.singUp(body).pipe(
-      tap((res: any) => {
-
-      if (res.status === 201) {
-        localStorage.setItem('token', res.tpken);
-        this._snackBar.openSuccessSnackBar('Thanks for your registration', 'close')
-        this._router.navigate(['/login'])};
-    }),
-    catchError(({error}: HttpErrorResponse) => {
-      this._snackBar.openErrorSnackBar(error.message, 'close');
-      return of(error.message)
-    })
-    ).subscribe();
-    this.subscriptions$.push(request$)
+    const request$ = this._registrationService
+      .singUp(body)
+      .pipe(
+        tap((res: any) => {
+          if (res.status === 201) {
+            localStorage.setItem('token', res.token); // TODO tpken ???? We don't habe token here
+            this._snackBar.openSuccessSnackBar(
+              'Thanks for your registration',
+              'close'
+            );
+            this._router.navigate(['/login']);
+          } // TODO need to change flow
+        }),
+        catchError(({ error }: HttpErrorResponse) => {
+          this._snackBar.openErrorSnackBar(error.message, 'close');
+          return of(error.message);
+        })
+      )
+      .subscribe();
+    this.subscriptions$.push(request$);
   }
 
   singupByGoogle() {
-    const requestGoogle$ = this._registrationService.googleLogin().pipe(tap((res: any) => {
-    })).subscribe();
-    this.subscriptions$.push(requestGoogle$)
-
+    const requestGoogle$ = this._registrationService
+      .googleLogin()
+      .pipe()
+      .subscribe();
+    this.subscriptions$.push(requestGoogle$);
   }
 
   checkPasswords() {
-    return this.myGroup.controls['confirmPass'].value !== this.myGroup.controls['password'].value
+    return (
+      this.myGroup.controls['confirmPass'].value !==
+      this.myGroup.controls['password'].value
+    );
   }
 
   ngOnDestroy(): void {
-    this.subscriptions$.forEach(s$ => {
+    this.subscriptions$.forEach((s$) => {
       s$.unsubscribe();
     });
   }
